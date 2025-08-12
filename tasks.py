1271ee1236d67164511faefff06397372c3cb480
Janu <janu.linde@gmail.com>
2025-08-12 21:42:59 +0200

    [UPDATE] Removed comment tags in tasks.py.

--- tasks.py
+++ tasks.py
@@ -15,12 +15,12 @@ def order_robots_from_RobotSpareBin():
     Creates ZIP archive of the receipts and the images.
     """
 
-    browser.configure(slowmo=800)
+    browser.configure(slowmo=200)
 
-    # open_robot_order_website()
-    # get_orders()
-    # give_up_consitutional_rights()
-    # order_all()
+    open_robot_order_website()
+    get_orders()
+    give_up_consitutional_rights()
+    order_all()
     archieve_receipts()
 
 
